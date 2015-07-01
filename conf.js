b0097947b8b4eca354c4ae228ea5f9e829f70bd6
Julian Veling <julian_veling@hotmail.com>
2015-07-01 20:01:38 +0100

    julian branch

--- conf.js
+++
@@ -1,8 +0,0 @@
-exports.config = {
-  seleniumAddress: "http://127.0.0.1:4444/wd/hub ",
-  baseUrl: 'http://localhost:3000',
-  specs: ['spec/features/*Spec.js'],
-  capabilities: {
-    'browserName': 'firefox'
-  }
-};
